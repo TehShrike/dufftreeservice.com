7ef73658f5be45da2aec424448c20a27f49f5f25
Josh Duff <me@JoshDuff.com>
2025-11-21 15:24:47 -0600

    Hey look, standalone pages for services

--- content/titles.js
+++ content/titles.js
@@ -2,4 +2,9 @@ export default {
 	about: 'About',
 	why_us: 'Why Us',
 	service_area: 'Service Area',
+	limb_removal: 'Limb Removal',
+	tree_removal: 'Tree Removal',
+	tree_trimming: 'Tree Trimming',
+	tree_planting: 'Tree Planting',
+	more_services: 'Other Services',
 }
