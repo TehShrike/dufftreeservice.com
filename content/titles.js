c3fd8b7fec3d7e5698ef0ab65963e48671ebde77
Josh Duff <me@JoshDuff.com>
2025-12-29 15:29:21 -0600

    social links in the site map, images on the service pages, arbostar form on the pages without estimators

--- content/titles.js
+++ content/titles.js
@@ -2,9 +2,12 @@ export default {
 	about: 'About',
 	why_us: 'Why Us',
 	service_area: 'Service Area',
-	limb_removal: 'Limb Removal',
+	services: 'Services',
 	tree_removal: 'Tree Removal',
 	tree_trimming: 'Tree Trimming',
 	tree_planting: 'Tree Planting',
+	arborist_consultation: 'Arborist Consultation',
+	injection: 'Injection',
+	cabling: 'Tree Cabling',
 	more_services: 'Other Services',
 }
